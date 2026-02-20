e02fca15c8fb1da07fd9baf3882aa4e27fa2d783
eliezerraj <eliezerraj@gmail.com>
2026-02-19 22:20:37 -0300

    fix:otel metrics path

--- assets/siege/01_inventory.sh
+++ assets/siege/01_inventory.sh
@@ -3,7 +3,6 @@
 # variabels
 
 export AUTH_TOKEN=
-
 export URL_HOST=https://go-api-global.architecture.caradhras.io/inventory
 
 #-----------------------------------------------------
@@ -29,6 +28,8 @@ STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" "$URL_GET" \
 
 if echo "$STATUS_CODE" | grep -q "HTTP:200"; then
   	echo "HTTP:200 GET /inventory/product/MOBILE-${RANDOM_INV}"
+elif echo "$STATUS_CODE" | grep -q "HTTP:404"; then	
+	echo -e "\e[38;2;255;165;0m** ERROR $STATUS_CODE ==> /inventory/product/MOBILE-${RANDOM_INV}\e[0m"
 else
 	echo -e "\e[31m** ERROR $STATUS_CODE ==> /inventory/product/MOBILE-${RANDOM_INV}\e[0m"
 fi
@@ -46,15 +47,17 @@ STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" -X PUT "$URL_PUT" \
 
 if echo "$STATUS_CODE" | grep -q "HTTP:200"; then
   	echo "HTTP:200 PUT inventory/product/MOBILE-${RANDOM_INV}"
+elif echo "$STATUS_CODE" | grep -q "HTTP:404"; then	
+	echo -e "\e[38;2;255;165;0m** ERROR $STATUS_CODE ==> inventory/product/MOBILE-${RANDOM_INV}\e[0m"	
 else
 	echo -e "\e[31m** ERROR $STATUS_CODE ==> inventory/product/MOBILE-${RANDOM_INV}\e[0m"
 fi
 
 #------------------------------------------
-RANDOM_INV=$((RANDOM % 999 + 1))
+RANDOM_INV=$((RANDOM % 9999 + 1))
 
 URL_POST="${URL_HOST}/product"
-PAYLOAD='{"sku":"WINE-'$RANDOM_INV'", "type":"BEVERAGE", "name": "WINE '$RANDOM_INV'", "status":"IN-STOCK"}'
+PAYLOAD='{"sku":"GPU-'$RANDOM_INV'", "type":"ELETRONIC", "name": "GPU '$RANDOM_INV'", "status":"IN-STOCK"}'
 
 STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" "$URL_POST" \
 	--header "Content-Type: application/json" \
@@ -62,16 +65,18 @@ STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" "$URL_POST" \
 	--data "$PAYLOAD")
 
 if echo "$STATUS_CODE" | grep -q "HTTP:200"; then
-  	echo "HTTP:200 POST inventory/product/WINE-${RANDOM_INV}"
+  	echo "HTTP:200 POST inventory/product/GPU-${RANDOM_INV}"
+elif echo "$STATUS_CODE" | grep -q "HTTP:400"; then	
+	echo -e "\e[38;2;255;165;0m** ERROR $STATUS_CODE ==> /product\e[0m"	
 else
 	echo -e "\e[31m** ERROR $STATUS_CODE ==> /product\e[0m"
 fi
 
 #------------------------------------------
-RANDOM_INV=$((RANDOM % 999 + 1))
+RANDOM_INV=$((RANDOM % 9999 + 1))
 
 URL_POST="${URL_HOST}/product"
-PAYLOAD='{"sku":"BEER-'$RANDOM_INV'", "type":"BEVERAGE", "name": "BEER '$RANDOM_INV'", "status":"IN-STOCK"}'
+PAYLOAD='{"sku":"LAPTOP-'$RANDOM_INV'", "type":"ELETRONIC", "name": "LAPTOP '$RANDOM_INV'", "status":"IN-STOCK"}'
 
 STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" "$URL_POST" \
 	--header "Content-Type: application/json" \
@@ -79,7 +84,9 @@ STATUS_CODE=$(curl -s -w " HTTP:%{http_code}" "$URL_POST" \
 	--data "$PAYLOAD")
 
 if echo "$STATUS_CODE" | grep -q "HTTP:200"; then
-  	echo "HTTP:200 POST inventory/product/BEER-${RANDOM_INV}"
+  	echo "HTTP:200 POST inventory/product/LAPTOP-${RANDOM_INV}"
+elif echo "$STATUS_CODE" | grep -q "HTTP:400"; then
+  	echo -e "\e[38;2;255;165;0m** ERROR $STATUS_CODE ==> /product\e[0m"
 else
 	echo -e "\e[31m** ERROR $STATUS_CODE ==> /product\e[0m"
 fi
